1a90c09aa89c56db981d76646c58cd7e7e8cd78b
Asel <asel.peiris@arimaclanka.com>
2022-08-18 08:45:12 +0530

    Infinite scroll functionality

--- src/App.js
+++ src/App.js
@@ -3,22 +3,10 @@ import './App.css';
 import Slider from './components/Carousel/Carousel';
 
 const App = () => {
-  const [slides, setSlides] = useState([]);
-  useEffect(()=> {
-    fetch(`http://localhost:3600/carousels?count=5`)
-    .then((response) => response.json())
-    .then((data) => {
-       console.log(data.data);
-       setSlides(data.data);
-    })
-    .catch((err) => {
-       console.log(err.message);
-    });
-  }, [])
 
 return (
   <div className="App">
-    <Slider slides={slides}/>
+    <Slider slide_count={5} infinte={false}/>
   </div>
 );
 };
