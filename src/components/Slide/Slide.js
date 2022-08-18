e3fc0cd2f287016b53bd3509022bfce5b319b7f0
Asel <asel.peiris@arimaclanka.com>
2022-08-18 07:24:22 +0530

    Adding Text Components

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,10 +1,17 @@
 import './Slide.css'
 import React from 'react'
+import Text from '../Text/Text'
+import { withGeneralStyling, withHeaderStyling } from '../Text/withHeaderStyling'
+const HeaderText = withHeaderStyling(Text)
+const SubTitle = withGeneralStyling(Text)
+
 
 export default function Slide({slide}) {
-  return (
-    <div className="slide-wrapper">
-        <img src={slide.image} className='slide-image' alt=""/>
-    </div>
-  )
+    return (
+        <div className="slide-wrapper">
+            <img src={slide.image} className='slide-image' alt=""/>
+            <HeaderText content='hello title'/>
+            <SubTitle content='worrld'/>
+        </div>
+    )
 }
