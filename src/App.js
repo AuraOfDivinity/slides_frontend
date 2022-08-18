85fa473d81e2716d7d6df9d2bc0e0acffdd88fb8
Asel <asel.peiris@arimaclanka.com>
2022-08-18 08:08:25 +0530

    Removing dummy data.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Slider from './components/Carousel/Carousel';
-import axios from "axios";
-
-const ImageData = [
-  {
-    image:
-      "https://images.unsplash.com/photo-1546190255-451a91afc548?ixlib=rb-1.2.1"
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1591348122449-02525d70379b?ixlib=rb-1.2.1"
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1548802673-380ab8ebc7b7?ixlib=rb-1.2.1"
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1577023311546-cdc07a8454d9?ixlib=rb-1.2.1"
-  }
-];
 
 const App = () => {
   const [slides, setSlides] = useState([]);
